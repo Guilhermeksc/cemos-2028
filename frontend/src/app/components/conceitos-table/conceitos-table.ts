046a1ae7c2869b22957123bb3205cc56299469f9
GuilhermeKsc <guilhermekscampos@gmail.com>
2025-10-15 22:58:42 -0300

    att

--- frontend/src/app/components/conceitos-table/conceitos-table.ts
+++ frontend/src/app/components/conceitos-table/conceitos-table.ts
@@ -1,11 +1,23 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { Conceitos } from '../../interfaces/informacoes.interface';
 
 @Component({
   selector: 'app-conceitos-table',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatCardModule,
+    MatChipsModule,
+    MatIconModule,
+    MatTooltipModule
+  ],
   templateUrl: './conceitos-table.html',
   styleUrls: ['./conceitos-table.scss']
 })
@@ -15,7 +27,17 @@ export class ConceitosTableComponent {
   @Input() title: string = 'Conceitos';
   @Input() emptyMessage: string = 'Nenhum conceito encontrado';
 
+  // Definir as colunas da tabela
+  displayedColumns: string[] = ['titulo', 'palavra-chave', 'descricao', 'assunto', 'bibliografia', 'prova'];
+
   constructor() {}
+
+  ngOnInit() {
+    // Ajustar colunas baseado nas props
+    if (!this.showBibliografia) {
+      this.displayedColumns = this.displayedColumns.filter(col => col !== 'bibliografia');
+    }
+  }
 
   /**
    * Verifica se o conceito caiu em prova e retorna o ano
