79bdb239308905b520487b5df5ff7aedfb99bb49
GuilhermeKSc <guilhermekscampos@gmail.com>
2025-11-21 23:41:10 -0300

    att

--- frontend-cemos/src/app/modules/app8-direito/app8-direito-bibliografia/lei-da-guerra/lei-da-guerra.ts
+++ frontend-cemos/src/app/modules/app8-direito/app8-direito-bibliografia/lei-da-guerra/lei-da-guerra.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { LivroIndividual } from '../../../../components/livro-individual/livro-individual';
 
 @Component({
   selector: 'app-lei-da-guerra',
-  imports: [],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    LivroIndividual
+  ],
   templateUrl: './lei-da-guerra.html',
   styleUrl: './lei-da-guerra.scss'
 })
-export class LeiDaGuerra {
+export class LeiDaGuerra implements OnInit {
+  // Configuração do LivroIndividual
+  contentPath: string = 'assets/content/direito/2-lei-da-guerra';
+  fileNames: string[] = [
+    // Adicione aqui os arquivos markdown do livro "A Lei da Guerra"
+    // Exemplo: 'cap1.md', 'cap2.md', etc.
+  ];
+  backRoute: string = '/home/app8-direito/bibliografia';
+  backLabel: string = 'Bibliografia';
 
+  ngOnInit() {
+    // Inicialização do componente
+  }
 }
