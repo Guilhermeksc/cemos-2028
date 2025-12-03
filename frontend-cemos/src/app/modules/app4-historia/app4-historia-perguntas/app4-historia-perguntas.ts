13b6dad28ed8503a3cd1dcb728035cf7a6dd8309
Guilhermeksc <guilhermekscampos@gmail.com>
2025-12-03 15:52:34 -0300

    att

--- frontend-cemos/src/app/modules/app4-historia/app4-historia-perguntas/app4-historia-perguntas.ts
+++ frontend-cemos/src/app/modules/app4-historia/app4-historia-perguntas/app4-historia-perguntas.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Perguntas } from '../../../components/perguntas/perguntas';
 
 @Component({
   selector: 'app-app4-historia-perguntas',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, Perguntas],
   templateUrl: './app4-historia-perguntas.html',
   styleUrl: './app4-historia-perguntas.css'
 })
-export class App4HistoriaPerguntas {
+export class App4HistoriaPerguntas implements OnInit {
+  // Bibliografias disponíveis para este módulo
+  readonly bibliografiasDisponiveisIds: number[] = [5, 6, 7, 8];
+    
+  // Rota para voltar à bibliografia
+  bibliografiaPath = '/home/app4-historia/bibliografia';
+  
+  pageTitle = 'Perguntas de História';
 
+  ngOnInit() {
+    console.log('Módulo de Perguntas - História iniciado');
+    console.log('Bibliografias disponíveis:', this.bibliografiasDisponiveisIds);
+  }
+
+  onSimuladoStarted() {
+    console.log('Simulado de História iniciado');
+  }
 }
