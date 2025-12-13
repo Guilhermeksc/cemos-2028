81ad111d457ed6a703914dfd27204e70571ae357
GuilhermeKSc <guilhermekscampos@gmail.com>
2025-12-13 00:11:30 -0300

    att

--- frontend-cemos/src/app/modules/app1-intendencia/app1-intendencia-flashcards/app1-intendencia-flashcards.ts
+++ frontend-cemos/src/app/modules/app1-intendencia/app1-intendencia-flashcards/app1-intendencia-flashcards.ts
@@ -10,7 +10,9 @@ import { FlashCardsComponent } from '../../../components/flash-cards/flash-cards
 })
 export class App1IntendenciaFlashcards {
 
-  bibliografiaIds: number[] = [18];
+  bibliografiaIds: number[] = [36, 37, 38, 
+    39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53,
+    54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68];
 
   // Rota para voltar à bibliografia
   bibliografiaPath = '/home/app1-intendencia/bibliografia';
--- frontend-cemos/src/app/modules/app1-intendencia/app1-intendencia-perguntas/app1-intendencia-perguntas.ts
+++ frontend-cemos/src/app/modules/app1-intendencia/app1-intendencia-perguntas/app1-intendencia-perguntas.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Perguntas } from '../../../components/perguntas/perguntas';
 
 @Component({
   selector: 'app-app1-intendencia-perguntas',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, Perguntas],
   templateUrl: './app1-intendencia-perguntas.html',
-  styleUrl: './app1-intendencia-perguntas.css'
+  styleUrl: './app1-intendencia-perguntas.scss'
 })
-export class App1IntendenciaPerguntas {
+export class App1IntendenciaPerguntas implements OnInit {
+  // Bibliografias disponíveis para este módulo
+  readonly bibliografiasDisponiveisIds: number[] = [36, 37, 38, 
+    39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53,
+    54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68];
+    
+  // Rota para voltar à bibliografia
+  bibliografiaPath = '/home/app1-intendencia/bibliografia';
+  
+  pageTitle = 'Perguntas de Intendência';
 
+  ngOnInit() {
+    console.log('Módulo de Perguntas - Intendência iniciado');
+    console.log('Bibliografias disponíveis:', this.bibliografiasDisponiveisIds);
+  }
+
+  onSimuladoStarted() {
+    console.log('Simulado de Intendência iniciado');
+  }
 }
