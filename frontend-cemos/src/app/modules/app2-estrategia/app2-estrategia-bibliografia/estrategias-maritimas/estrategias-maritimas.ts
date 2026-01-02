a5667783412edf37d9e45f9a353504ba22ffa5e5
GuilhermeKSc <guilhermekscampos@gmail.com>
2026-01-01 22:00:10 -0300

    att

--- frontend-cemos/src/app/modules/app2-estrategia/app2-estrategia-bibliografia/estrategias-maritimas/estrategias-maritimas.ts
+++ frontend-cemos/src/app/modules/app2-estrategia/app2-estrategia-bibliografia/estrategias-maritimas/estrategias-maritimas.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { LivroIndividual } from '../../../../components/livro-individual/livro-individual';
 
 @Component({
   selector: 'app-estrategias-maritimas',
-  imports: [],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    LivroIndividual
+  ],
   templateUrl: './estrategias-maritimas.html',
-  styleUrl: './estrategias-maritimas.scss'
+  styleUrl: './estrategias-maritimas.scss',
+  encapsulation: ViewEncapsulation.None
 })
-export class EstrategiasMaritimas {
+export class EstrategiasMaritimas implements OnInit {
+// Configuração do LivroIndividual
+contentPath: string = 'assets/content/estrategia/2-estrategias-maritimas';
+fileNames: string[] = [
+  'cap1.md',
+  'cap4.md',
+  'cap5.md',
+  'cap6.md',  
+];
+backRoute: string = '/home/app2-estrategia/bibliografia';
+backLabel: string = 'Bibliografia';
 
+ngOnInit() {
+  // Inicialização do componente
+}
 }
